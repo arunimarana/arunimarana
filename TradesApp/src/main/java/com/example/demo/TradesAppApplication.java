171e4dc3b52ff0396e5ceb1c43c0a46727fad81a
arunimarana <88302477+arunimarana@users.noreply.github.com>
2026-05-07 12:16:48 +0530

    Add files via upload

---
+++ TradesApp/src/main/java/com/example/demo/TradesAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesAppApplication.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
